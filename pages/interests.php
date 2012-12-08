e1e3e96767542a61851dfd3aaba248232f0960a2
P. Envall <petter.envall@gmail.com>
2012-12-08 17:51:33 +0100

    Knut skrev om sin ålder

--- pages/interests.php
+++ pages/interests.php
@@ -2,6 +2,8 @@
 	Mina intressen
 </h3>
 
-<ul>
+<p>
+	Jag gillar <a href="//minecraft.net/">Minecraft</a>. Här är en bild av kartan i en värld där jag spelar ibland:
+</p>
 	
-</ul>
+<iframe src="//mc.envall.se/maps/mc_npup/" frameborder="0" width="650" height="400"></iframe>
--- pages/me.php
+++ pages/me.php
@@ -1,3 +1,9 @@
 <h3>
 	Jag är Knut.
 </h3>
+	
+<p>
+	
+hjsanjagärsexår
+	
+</p>
